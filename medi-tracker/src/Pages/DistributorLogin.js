ee99e75510a5becc2e7f2184c21cceb1c2aecb29
rudransh02 <rudranshsinghrajpurohit@gmai.com>
2023-05-02 23:12:58 +0530

    Frontend Commmit Of Forms For Collection Of The Transaction Data

--- medi-tracker/src/Pages/DistributorLogin.js
+++ medi-tracker/src/Pages/DistributorLogin.js
@@ -1,8 +1,75 @@
+import Card from '@mui/material/Card';
+import { TextField } from '@mui/material';
+import Button from '@mui/material/Button';
+
 const DistributorLogin = () =>
 {
     return (
         <div className="DistributorLogin">
             <h1>Distributor Login Page</h1>
+
+            <Card sx={{
+                border: 'none',
+                textAlign: 'center',
+                marginLeft: '350px',
+                marginRight: '350px'
+            }} variant='outlined'>
+
+                <TextField required sx={{
+                    position: 'relative',
+                    marginTop: '40px',
+                    marginBottom: '40px',
+                    display: 'grid'
+            }} label="Full Name"/>
+
+                <TextField required sx={{
+                    marginTop: '40px',
+                    marginBottom: '40px',
+                    display: 'grid'
+            }} label="Email"/>
+
+                <TextField required sx={{
+                    marginTop: '40px',
+                    marginBottom: '40px',
+                    display: 'grid'
+            }} label="Cloth Category"/>
+
+                <TextField required sx={{
+                    marginTop: '40px',
+                    marginBottom: '40px',
+                    display: 'grid'
+            }} label="Cloth Material"/>
+
+                <TextField required sx={{
+                    marginTop: '40px',
+                    marginBottom: '40px',
+                    display: 'grid'
+            }} label="Cloth Color"/>
+
+                <TextField required sx={{
+                    marginTop: '40px',
+                    marginBottom: '40px',
+                    display: 'grid'
+            }} label="Cloth Size"/>
+
+                <TextField required sx={{
+                    marginTop: '40px',
+                    marginBottom: '40px',
+                    display: 'grid',
+                }} label="Cloth Cost"/>
+
+                <Button sx={{
+                     "&:hover": {
+                        border: "2px solid",
+                        backgroundColor: '#D3DEFF'
+                    },
+                    paddingRight: '50px',
+                    paddingLeft: '50px',
+                    border: 'solid 2px',
+            }} size='large' variant='outlined'>SUMBIT</Button>
+
+            </Card>
+
         </div>
     );
 }
